b8c68ec0fcd4a60577a4af537543e1499d2ccbcf
emrekahraman <43474063+emre-kahraman@users.noreply.github.com>
2023-01-04 17:16:54 +0300

    Add itShouldCreateEmailRequest

--- order-service/src/test/java/com/example/orderservice/OrderServiceTests.java
+++ order-service/src/test/java/com/example/orderservice/OrderServiceTests.java
@@ -1,6 +1,7 @@
 package com.example.orderservice;
 
 import com.example.cartservice.dto.CreateOrderRequest;
+import com.example.orderservice.dto.CreateEmailRequest;
 import com.example.orderservice.dto.OrderDTO;
 import com.example.orderservice.entity.Order;
 import com.example.orderservice.repository.OrderRepository;
@@ -12,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -20,6 +22,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,6 +33,9 @@ public class OrderServiceTests {
 
     @Mock
     OrderRepository orderRepository;
+
+    @Mock
+    KafkaTemplate<String, CreateEmailRequest> kafkaTemplate;
 
     @Test
     public void itShouldGetAllOrders(){
@@ -76,5 +82,18 @@ public class OrderServiceTests {
 
         assertEquals(savedOrder.getUserId(), createOrderRequest.getUserId());
         assertEquals(savedOrder.getUserName(), createOrderRequest.getUserName());
+    }
+
+    @Test
+    public void itShouldCreateEmailRequest(){
+        Order order = Order.builder().userId("1").userName("test").userLastName("test").email("test@gmail.com").address("test")
+                .cartItems(new HashSet<>()).date(new Date()).build();
+
+        when(kafkaTemplate.send(eq("emails"), any(), any())).thenReturn(null);
+
+        CreateEmailRequest createEmailRequest = orderService.createEmailRequest(order);
+
+        assertEquals(createEmailRequest.getEmail(), order.getEmail());
+        assertEquals(createEmailRequest.getUserName(), order.getUserName());
     }
 }
